7df3bcad768ed4e022140c902ca16264c2fede18
Jason Schmitt <schmitty890@gmail.com>
2018-04-10 11:56:01 -0400

    minor navbar feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log('main.js');
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+  
